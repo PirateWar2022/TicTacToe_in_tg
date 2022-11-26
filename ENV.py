4efe801265ce65623d830bd06a6e91df549f51dd
PirateWar2022 <103024307+PirateWar2022@users.noreply.github.com>
2022-11-26 13:40:14 +0200

    Update ENV.py

--- ENV.py
+++ ENV.py
@@ -3,4 +3,4 @@ from os import environ
 
 
 class Tgtoken:
-     TOKEN: Final = environ.get('token' ,'5696217045:AAEBwGoqkmyDe1c7Sr0iexpTguAuR7eIQOA')
+     TOKEN: Final = environ.get('token' ,'token')
